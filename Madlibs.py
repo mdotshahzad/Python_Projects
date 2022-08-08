3ae99f7de3c3c005e1b541cf00c76923279ce573
mdotshahzad <mdotshahzad@gmail.com>
2022-08-08 13:11:59 +0500

    Madlib completed

--- Madlibs.py
+++ Madlibs.py
@@ -1,12 +1,12 @@
-# This project is about the Madlib game that will fill the blacks.
-#Updated python ext on git hub
+# This is Madlis Project.
 
+Word1 = input ("BlankedWord1: ")
+Word2 = input ("BlankedWord2: ")
+Word3 = input ("BlankedWord3: ")
 
-# I am making these changes on VS.
+Madlib = f"I am writing these paragraphg for {Word1} project. This {Word2} will help to learn  \
+basic {Word1} programming specially in {Word3} manipulation."
 
-#Will this required another changes on devce this time?
+print(Madlib)
 
-print("Han")
-
-print("I am printing it from commad Line")
 
